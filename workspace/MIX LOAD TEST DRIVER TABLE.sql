8c11010f7d0cb3acb85b676f18e18761e8616700
PrimSonn <crygit@hotmail.com>
2018-03-20 21:07:47 +0900

    sql script now works fine. beaware that PW seems to be case sensitive

--- workspace/MIX LOAD TEST DRIVER TABLE.sql
+++ workspace/MIX LOAD TEST DRIVER TABLE.sql
@@ -1,7 +1,7 @@
-SET TERMOUT OFF
-SET ECHO OFF
+--SET TERMOUT OFF
+--SET ECHO OFF
 
-CONNECT SCOTT/TIGER
+CONNECT SCOTT/tiger
 
 drop table MOVIES;
 drop table ACCOUNTS;
@@ -12,8 +12,8 @@ drop table ACCOUNT_IMGS;
 create table MOVIES (IDX number, TITLE varchar2(20));
 create table ACCOUNTS (IDX number, ID varchar2(20));
 create table MOVIE_COMMENTS (IDX number, TITLE varchar2(20));
-Rem create table MOVIE_IMGS (TYPE varchar2(20));
-Rem create table ACCOUNT_IMGS (TYPE varchar2(20));
+-- create table MOVIE_IMGS (TYPE varchar2(20));
+-- create table ACCOUNT_IMGS (TYPE varchar2(20));
 
 insert all
 	into MOVIES (IDX, TITLE) values (1,'Movie1')
@@ -22,13 +22,13 @@ insert all
 	into ACCOUNTS (IDX, ID) values (2, '아이디2')
 	into MOVIE_COMMENTS (IDX, TITLE) values (1, 'Comment1')
 	into MOVIE_COMMENTS (IDX, TITLE) values (2, '아이디2')
-Rem 	into MOVIE_IMGS (TYPE) values ('thumb')
-Rem 	into MOVIE_IMGS (TYPE) values ('poster')
-Rem 	into ACCOUNT_IMGS (TYPE) values ('poster')
-Rem 	into ACCOUNT_IMGS (TYPE) values ('back')
+--	into MOVIE_IMGS (TYPE) values ('thumb')
+-- 	into MOVIE_IMGS (TYPE) values ('poster')
+--	into ACCOUNT_IMGS (TYPE) values ('poster')
+--	into ACCOUNT_IMGS (TYPE) values ('back')
 select 1 from DUAL;
 
 commit;
 
-SET TERMOUT ON
-SET ECHO ON
+--SET TERMOUT ON
+--SET ECHO ON
